3829b97ad6522c9999ee303a09ece7818f5c54b8
Victoria <95590803+anarayner@users.noreply.github.com>
2022-01-24 17:25:10 -0500

    script

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 const result = document.getElementById("result");
-const sound = document.getElementById("sound");
+const sound = document.getElementById("audio");
 const button = document.getElementById("button");
 
 button.addEventListener("click", () => {
   let searchWord = document.getElementById("serch_box__word").value;
   console.log(searchWord);
+
   fetch(`${url}${searchWord}`)
     .then((response) => response.json())
     .then((word) => {
       console.log(word);
       result.innerHTML = ` 
         <div class="result__word">
-        <h3>Sample</h3>
-        <button class="result__volume">
+        <h3>${searchWord}</h3>
+        <button class="result__volume" onclick="playSound()">
           <i class="fas fa-volume-down"></i>
         </button>
       </div>
       <div class="details">
-        <p>pos</p>
-        <p>/sample/</p>
+        <p>${word[0].meanings[0].partOfSpeech}</p>
+        <p>/${word[0].phonetic}/</p>
       </div>
           <p class="word-meaning">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <span>def:</span> ${
+            word[0].meanings[0].definitions[0].definition
+          }<br/><span>def:</span>  ${
+        word[0].meanings[0].definitions[1].definition
+      }
         </p>
         <p class="word-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          suscipit euismod magna, sit amet varius ante lacinia eget. Donec
-          pretium tristique vulputate.
+         ${word[0].meanings[0].definitions[0].example || ""}<br/>${
+        word[0].meanings[0].definitions[1].example || ""
+      }
         </p>`;
+      sound.setAttribute("src", `http:${word[0].phonetics[0].audio}`);
+      console.log(sound);
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = ` 
+        <div class="result__word">
+        <h3>Sorry, we couldn't find definitions for the word you were looking for.</h3>`;
     });
 });
+
+function playSound() {
+  sound.play();
+}
